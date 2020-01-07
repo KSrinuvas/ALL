3b3851621bf03544380df041be652922878e0f97
SRMHP2 <Srinuvas935@gmail.com>
2020-01-07 09:56:42 +0530

    lic file

---
+++ lic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3 
+import re;
+
+##-----------------------------------------------------------
+## Description : To get the tool details and build the dict
+##-----------------------------------------------------------
+
+
+## read the file lic_log
+fobj = open('/home/srm/Downloads/lic_log');
+
+for i in fobj.readlines():
+			i = i.strip(); ## remove the new line charaters 
+		#	print(i);
+
+
+			
+					
